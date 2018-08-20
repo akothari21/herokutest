9bde78b70afcb7993fed6aca8b7c4180edcc18bd
akothari21 <31791036+akothari21@users.noreply.github.com>
2018-08-20 23:47:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/:name", function(req, res){
 
 })
  app.get("*", function(req, res){
-     res.send("hey there! Welcome to  BitByBit. for more details drop a mail to contact.bitbybit@gmail.com");
+     res.send("hey there! Welcome to <h1>BitByBit</h1>. for more details drop a mail to contact.bitbybit@gmail.com");
 
  });
 
